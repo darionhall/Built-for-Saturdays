e62c7b493dce70cc0dbb4a5ed56f3ec86e0883dc
Darion Hall <darionhall@gmail.com>
2016-06-15 13:54:30 -0400

    added files to updated app

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,14 @@
   'use strict';
 
   angular
-    .module('builtForSaturdays', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+    .module('stadiumTest', ['ngAnimate',
+                            'ngCookies',
+                            'ngTouch',
+                            'ngSanitize',
+                            'ngMessages',
+                            'ngAria',
+                            'ui.router',
+                            'ui.bootstrap',
+                            'toastr']);
 
 })();
